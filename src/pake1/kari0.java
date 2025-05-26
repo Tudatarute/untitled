5c9957ec8d2092673686f57d6cfdffba740800f1
241047 <241047@std.hi-joho.ac.jp>
2025-05-26 11:37:49 +0900

    確かに致しました。

--- src/pake1/kari0.java
+++ src/pake1/kari0.java
@@ -1,3 +1,5 @@
+package pake1;
+
 public class kari0 {
     public static  void main(String[] args) {
         System.out.println("津田");
---
+++ src/reviews/reviews2/Review15.java
@@ -0,0 +1,15 @@
+public class Review15 {
+    public static void main(String[] args) {
+       int num = 1;
+       if(num > 0){
+           if(num % 2 == 0){
+               System.out.println("偶数");
+           }else{
+               System.out.println("奇数");
+           }
+       }else{
+           System.out.println("無効な入力です。");
+       }
+    }
+}
+
